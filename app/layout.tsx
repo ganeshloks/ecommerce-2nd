a65af721421c5441bbbdccc67ca5b5eeed0411f5
ganeshloks <161136362+ganeshloks@users.noreply.github.com>
2024-03-24 17:49:53 -0400

    2nd commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,11 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "./components/Navbar";
+import CartProvider from "./components/providers";
+import ShoppingCartModal from "./components/ShoopingCartModal";
+
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +21,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+       <CartProvider>
+       <Navbar />
+       <ShoppingCartModal />
+        {children}
+       </CartProvider>
+        </body>
     </html>
   );
 }
